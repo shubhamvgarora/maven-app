a1d74f3119a788c2601969c2240d5bae44aa035c
Shubham Arora <shubham@devops-idle>
2024-09-20 03:05:51 +0000

    Add Main.java class

---
+++ src/main/java/in/aseemcloud/Main.java
@@ -0,0 +1,8 @@
+package in.aseemcloud;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // Add more logic as needed.
+    }
+}
